3b5248eada877a3102736872abaf2c25a5df00e0
Subham Khandelwal <subhamkharwal@gmail.com>
2021-06-13 13:54:19 +0530

    Changed Pumpkin to Fruits

--- fruits.py
+++ fruits.py
@@ -1,1 +1,1 @@
-fruits = ['apple', 'dragonfruit', 'peach', 'banana', 'grape', 'apple', 'peach', 'watermelon', 'grape', 'grape']
+fruits = ['pumpkin', 'apple', 'dragonfruit', 'peach', 'banana', 'grape', 'apple', 'peach', 'watermelon', 'grape', 'grape']
